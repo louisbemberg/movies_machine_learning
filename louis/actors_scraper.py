9cc7b72d331353b8f1b5aff9f4c04968fdc35569
louis bemberg <louis.bemberg@gmail.com>
2021-11-21 15:20:11 -0800

    scraped top actors and  directors and exported to CSV to prevent IP block

---
+++ louis/actors_scraper.py
@@ -0,0 +1,23 @@
+import requests
+from bs4 import BeautifulSoup
+import numpy as np
+
+
+# TOP ACTOR SCRAPING
+url1 = "https://www.imdb.com/list/ls050274118/"
+page1 = requests.get(url1)
+soup1 = BeautifulSoup(page1.content, "html.parser")
+actor_cards1 = soup1.find_all("h3", class_="lister-item-header")
+
+actors = []
+
+for actor in actor_cards1:
+    actor_name = actor.text[5:].strip()
+    actors.append(actor_name)
+
+print(actors)
+
+np.savetxt("best_actors.csv", 
+           actors,
+           delimiter =", ", 
+           fmt ='% s')
---
+++ louis/directors_scraper.py
@@ -0,0 +1,35 @@
+import requests
+from bs4 import BeautifulSoup
+import numpy as np
+
+
+# TOP DIRECTOR SCRAPING
+url1 = "https://www.imdb.com/list/ls056848274/"
+page1 = requests.get(url1)
+soup1 = BeautifulSoup(page1.content, "html.parser")
+director_cards1 = soup1.find_all("h3", class_="lister-item-header")
+
+directors = []
+
+for director in director_cards1:
+    director_name = director.text[5:].strip()
+    directors.append(director_name)
+
+
+url2 = "https://www.imdb.com/list/ls056848274/?sort=list_order,asc&mode=detail&page=2"
+page2 = requests.get(url2)
+soup2 = BeautifulSoup(page2.content, "html.parser")
+director_cards2 = soup2.find_all("h3", class_="lister-item-header")
+
+for director in director_cards2:
+    director_name = director.text[5:].strip()
+    directors.append(director_name)
+
+
+print(directors)
+
+np.savetxt("best_directors.csv", 
+           directors,
+           delimiter =", ", 
+           fmt ='% s')
+
